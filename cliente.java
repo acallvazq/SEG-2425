16f3b2105293e1eb4e625fceb3a916c0c4fbfa7a
Alba Vazquez <runo-mola12@hotmail.es>
2024-09-20 10:42:18 +0200

    actualizado

---
+++ cliente.java
@@ -0,0 +1,12 @@
+
+
+public class cliente {
+    
+    //Atributos
+
+    //Clases
+
+    //Metodos
+
+
+}
---
+++ servidor.java
@@ -0,0 +1,9 @@
+
+
+public class servidor {
+    //Atributos
+
+    //Clases
+
+    //Metodos
+}
