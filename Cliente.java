670bcf11aa91bcb2481a942598521214ccfe4a6d
Alba Vazquez <runo-mola12@hotmail.es>
2024-11-27 17:32:51 +0100

    Cliente.java sin autenticar

--- Cliente.java
+++ Cliente.java
@@ -30,14 +30,14 @@ public class Cliente {
         ficheroTrustStore = raiz + "/keyStoreCliente/trustStoreClient.jce";
 
 		definirKeyStores();
-		iniciarConexionTLS(host, port);
+		//iniciarConexionTLS(host, port);
 
-		menu();   
+		menu(host, port);   
 
     }
 
     //Metodos
-	private static void menu(){
+	private static void menu(String host, int port){
 		String respuesta;
         Scanner scanner = new Scanner(System.in);
 		int salir = 1;
@@ -58,7 +58,7 @@ public class Cliente {
 
             switch(respuesta){
                 case "1":
-                    registrarDocumento();
+                    registrarDocumento(host, port);
                     break;
                 case "2":
                   //  recuperarDocumento();   
@@ -77,17 +77,55 @@ public class Cliente {
 	 *              Registrar un Documento
 	 *****************************************************/
 
-    private static void registrarDocumento(){
+    private static void registrarDocumento(String host, int port){
 		String pathFile = preguntaUsuario("Introduce el directorio del archivo que deseas enviar: ");  // /home/alba/24-25/SEG/SEG-2425/textosPrueba/textoclaro.txt
         
 		//Creación de socket
-        SSLSocket socket = iniciarConexionTLS("localhost",8090);
+        iniciarConexionTLS(host, port);
 		
 		System.out.println("Registrando documento...");
     }
 
+	private static String preguntaUsuario(String mensaje){
+        String respuesta;
+		Scanner scanner = new Scanner(System.in);
 
-    private static SSLSocket iniciarConexionTLS(String host, int port){
+		System.out.print(mensaje);
+        respuesta = scanner.nextLine();
+
+		return respuesta;
+    }
+
+
+	/******************************************************
+	 * definirKeyStores()
+	 *****************************************************/
+    
+	private static void definirKeyStores() {
+        // Almacen de credenciales
+		//System.out.println("keyStore:" + ficheroKeyStore);
+      //  System.setProperty("javax.net.ssl.keyStore", ficheroKeyStore);
+      //  System.setProperty("javax.net.ssl.keyStoreType", "JCEKS");
+      //  System.setProperty("javax.net.ssl.keyStorePassword", contrasinal);
+        
+        // Almacen de confianza
+		System.setProperty("javax.net.ssl.trustStore",          raiz + "/keyStoreCliente/trustStoreClient1.jce");
+		System.setProperty("javax.net.ssl.trustStoreType",     "JCEKS");
+		System.setProperty("javax.net.ssl.trustStorePassword", "criptonika");
+		//System.out.println("trustStore:" + ficheroTrustStore);
+        //System.setProperty("javax.net.ssl.trustStore", ficheroTrustStore);
+        //System.setProperty("javax.net.ssl.trustStoreType", "JCEKS");
+        //System.setProperty("javax.net.ssl.trustStorePassword", contrasinal);
+    }
+
+	/*******************************************************
+	 *              inicializar conexion TLS
+	 *******************************************************/
+
+
+
+	
+    private static void iniciarConexionTLS(String host, int port){
 		try{
 			SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
 
@@ -114,18 +152,18 @@ public class Cliente {
 							new OutputStreamWriter(
 							socket.getOutputStream())));
 
-			out.println("GET " + "/" + args[2]  + " "  + " HTTP/1.0");
+			//out.println("GET " + "/" + args[2]  + " "  + " HTTP/1.0");
 			out.println();
 			out.flush();
 
-			System.out.println("GET " + "/" + args[2]  + " " + "HTTP/1.0");
+			//System.out.println("GET " + "/" + args[2]  + " " + "HTTP/1.0");
 			/*
 			* Make sure there were no surprises
 			*/
 			if (out.checkError())
 				System.out.println("SSLSocketClient:  java.io.PrintWriter error");
 
-			/* Leer respuesta */
+			// Leer respuesta 
 			BufferedReader in = new BufferedReader(
 								new InputStreamReader(
 								socket.getInputStream()));
@@ -143,140 +181,9 @@ public class Cliente {
         }
 
     }
-
-
-	private static String preguntaUsuario(String mensaje){
-        String respuesta;
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print(mensaje);
-        respuesta = scanner.nextLine();
-
-		return respuesta;
-    }
-
-	/******************************************************
-	 * definirKeyStores()
-	 *****************************************************/
     
-	private static void definirKeyStores() {
-        // Almacen de credenciales
-        System.setProperty("javax.net.ssl.keyStore", ficheroKeyStore);
-        System.setProperty("javax.net.ssl.keyStoreType", "JCEKS");
-        System.setProperty("javax.net.ssl.keyStorePassword", contrasinal);
 
-        // Almacen de confianza
-        System.setProperty("javax.net.ssl.trustStore", ficheroTrustStore);
-        System.setProperty("javax.net.ssl.trustStoreType", "JCEKS");
-        System.setProperty("javax.net.ssl.trustStorePassword", contrasinal);
-    }
+
 }
 
 
-
-    /*
-    ------------------------     PENDIENTE DE REVISION     ------------------------
-    private SSLSocket iniciarConexionSSL(){
-        SSLSocket socket;
-        definirKeyStores();
-
-        // Crear Key Manager
-        try {
-            SSLSocketFactory factory = null;
-			SSLContext ctx;
-			KeyManagerFactory kmf;
-			KeyStore ks;
-
-			try {
-				ctx = SSLContext.getInstance("TLS");
-
-				// Definir el/los KeyManager.
-				kmf = KeyManagerFactory.getInstance("SunX509");
-				ks = KeyStore.getInstance("JCEKS");
-				ks.load(new FileInputStream(keyStorePathCliente), contrasinal);
-				kmf.init(ks, contrasinal);
-				
-				ctx.init(kmf.getKeyManagers(), null, null);
-
-				// Asignamos un socket al contexto.
-
-				factory = ctx.getSocketFactory();*/
-
-				/*********************************************************************
-				 * Suites del contexto
-				 *********************************************************************/
-	/*			System.out.println("******** CypherSuites Disponibles **********");
-				cipherSuites = factory.getSupportedCipherSuites();
-				for (int i = 0; i < cipherSuites.length; i++)
-					System.out.println(cipherSuites[i]);
-
-				// Suites habilitadas por defecto
-
-				System.out.println("******* CypherSuites Habilitadas por defecto **********");
-
-				String[] cipherSuitesDef = factory.getDefaultCipherSuites();
-				for (int i = 0; i < cipherSuitesDef.length; i++)
-					System.out.println(cipherSuitesDef[i]);
-
-			} catch (Exception e) {
-				throw new IOException(e.getMessage());
-			}
-
-			SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
-
-			String[] cipherSuitesHabilitadas = { "TLS_RSA_WITH_AES_128_CBC_SHA" };
-
-			System.out.println(cipherSuitesHabilitadas[0]);
-
-			socket.setEnabledCipherSuites(cipherSuitesHabilitadas);
-
-			System.out.println("****** CypherSuites Habilitadas  **********");
-
-			String[] cipherSuitesHabilSocket = socket.getEnabledCipherSuites();
-			for (int i = 0; i < cipherSuitesHabilSocket.length; i++)
-				System.out.println(cipherSuitesHabilSocket[i]);
-
-
-			System.out.println("\n*************************************************************");
-			System.out.println("  Comienzo SSL Handshake -- Cliente y Servidor Autenticados     ");
-			System.out.println("*************************************************************");
-
-			socket.startHandshake();
-
-			System.out.println("\n*************************************************************");
-			System.out.println("      Fin OK   SSL Handshake");
-			System.out.println("*************************************************************");
-
-			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-			
-			out.println("GET /" + path + " HTTP/1.1");
-			out.println();
-			out.flush();
-
-			if (out.checkError())
-				System.out.println("SSLSocketClient: java.io.PrintWriter error");*/
-
-			/* Leer respuesta */
-		/*	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			String inputLine;
-
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-
-			in.close();
-			out.close();
-			socket.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-    }
-
-
-
-    private static void recuperarDocumento(){
-        System.out.println("Recuperando el documento...");
-    }
-*/
